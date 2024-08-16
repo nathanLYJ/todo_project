4dc0ebf401f5466b96426cec73c32f0eeecfa368
nathanLYJ <nathanlyj@gmail.com>
2024-08-16 15:43:56 +0900

    💄 setting 설정ALLOWED_HOSTS 변경,

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-78u6pc80ghe$rw$w$adwu^q_$yvuqhkx3!dv$n%*+c--4@j=s^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://todo-project-xs8k.onrender.com', 'localhose', '127.0.0.1']
+ALLOWED_HOSTS = ['todo-project-xs8k.onrender.com', 'localhose', '127.0.0.1']
 
 
 # Application definition
